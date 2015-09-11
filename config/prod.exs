c6886d7d8b7cfa5446c977a8aef5a780aabe9877
jkgisme <incarnadin@gmail.com>
2015-09-10 23:21:00 -0500

    fix missing comma

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 config :square_square_backend, SquareSquareBackend.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "square-square-backend.herokuapp.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
